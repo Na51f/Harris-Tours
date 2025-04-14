7a398716f5d81d2b45061d64483c70fe27384b65
Nasif Mauthoor <git@nasifmauthoor.com>
2025-04-14 13:38:51 -0400

    Added more navbar interactivity

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -58,13 +58,15 @@ export default function Navbar() {
       <div className="flex flex-row gap-4">
         <svg xmlns="http://www.w3.org/2000/svg"
           width="24" height="24" viewBox="0 0 24 24" fill="none"
-          stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+          stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
+          className="hover:scale-110 transition-transform">
           <circle cx="11" cy="11" r="8"></circle>
           <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
         </svg>
         <svg xmlns="http://www.w3.org/2000/svg"
           width="24" height="24" viewBox="0 0 24 24" fill="none"
-          stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+          stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
+          className="hover:scale-110 transition-transform">
           <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
           <circle cx="12" cy="7" r="4"></circle>
         </svg>
